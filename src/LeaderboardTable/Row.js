f96bc895aa221e6e1756a40947c334e429677447
BATGIRL\sgalliance526 <sean.cube@yahoo.com>
2018-06-06 21:55:11 -0400

    Dynamically getting table data from json using axios

--- src/LeaderboardTable/Row.js
+++ src/LeaderboardTable/Row.js
@@ -1,36 +1,26 @@
 import React, {Component} from 'react';
 import axios from "axios/index";
-import campers from '../SampleData/campers';
 
 class Row extends Component {
     constructor(){
         super();
         this.state = {
-            data: campers.campers
+            tableData: []
         };
 
         this.sampleURL = 'http://localhost:4000/campers';
     }
 
-    /*componentDidMount(){
+    componentDidMount(){
         let self = this;
-        axios.get(this.sampleURL).then((response) => {
-            console.log(response);
-            self.setState((response.campers.map(person => {
-                return(
-                    <tr>
-                        <td>{person.id}</td>
-                        <td>{person.name}</td>
-                        <td>{person.total_points}</td>
-                        <td>{person.point_in_last_30_days}</td>
-                    </tr>
-                );
-            })));
+        axios.get(this.sampleURL, { responseType: 'json'}).then((response) => {
+            //console.log(response.data);
+            self.setState({tableData: response.data});
         });
-    };*/
+    };
 
     render() {
-        let rows = this.state.data.map(person => {
+        let rows = this.state.tableData.map(person => {
             return(
                 <tr>
                     <td>{person.id}</td>
